82d3df2ac8f413de4c20d32a08caf0bddf8b016f
csplr <33940018+richance@users.noreply.github.com>
2023-01-23 01:52:21 +0400

    Init

---
+++ config/network.tsx
@@ -0,0 +1,75 @@
+import { Network, Alchemy } from 'alchemy-sdk';
+import { Chain } from '@rainbow-me/rainbowkit';
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit'
+import { chain, configureChains, createClient, WagmiConfig, useContractWrite } from 'wagmi'
+import { alchemyProvider } from 'wagmi/providers/alchemy'
+import { publicProvider } from 'wagmi/providers/public'
+import { RainbowKitChainProvider } from '@rainbow-me/rainbowkit/dist/components/RainbowKitProvider/RainbowKitChainContext'
+import MenuItem from '@mui/material/MenuItem';
+import { useEffect, useState } from "react";
+import React from 'react';
+
+const FTMC: Chain = {
+  id: 250,
+  name: 'Fantom',
+  network: 'fantom',
+  iconUrl: 'https://cryptologos.cc/logos/fantom-ftm-logo.svg?v=023',
+  iconBackground: '#fff',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Fantom',
+    symbol: 'FTM',
+  },
+  rpcUrls: {
+    default: 'https://rpc.ftm.tools'//'https://rpc.ftm.tools/',
+  },
+  blockExplorers: {
+    default: { name: 'FTM', url: '	https://ftmscan.com/' },
+  },
+  testnet: false,
+};
+const FTMRPC = 'https://rpc.ftm.tools/'
+const FTMA = '0x0947ef8Bf078b8201013c77C39b5f0A5Bb8f58EC'
+const FTMN = '0xBE153Eb48062ba3892C10389844643d37cedA639'
+const FTMM = <MenuItem value={'0x0947ef8Bf078b8201013c77C39b5f0A5Bb8f58EC'}>Main</MenuItem>
+const BSCC: Chain = {
+  id: 56,
+  name: 'Binance Chain',
+  network: 'BSC',
+  iconUrl: 'https://cryptologos.cc/logos/bnb-bnb-logo.svg?v=023',
+  iconBackground: '#fff',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'BSC',
+    symbol: 'BNB',
+  },
+  rpcUrls: {
+    default: 'https://bsc-dataseed.binance.org/'//'https://rpc.ftm.tools/',
+  },
+  blockExplorers: {
+    default: { name: 'B', url: '	https://bscscan.com/' },
+  },
+  testnet: false,
+};
+const BSCRPC = 'https://bsc-dataseed.binance.org/'
+const BSCA = '0x18C519E0dA619d017908aFf504e782E381552620'
+const BSCN = '0xB8112446078378f0998FBf834D4683B6C8Ac08C7'
+const BSCM = [<MenuItem value={'0x18C519E0dA619d017908aFf504e782E381552620'}>Main</MenuItem>]
+const PolyM = [<MenuItem value={'0x00f0feed50dcdf57b4f1b532e8f5e7f291e0c84b'}>Main</MenuItem>,
+<MenuItem value={'0x3783b8DAf1B14d8e556ec1C681CEB0056157e810'}>Sub Wall</MenuItem>,
+]
+const PolyC = chain.polygon
+const PolyRPC = 'https://polygon-rpc.com'
+const PolyA = '0x00f0feed50dcdf57b4f1b532e8f5e7f291e0c84b'
+const PolyN = '0x99029716DEeE316894DC8ce4f55Ab066222AACe6'
+
+const envVars = {
+  chainn:PolyC,//chainn,
+  rpc: PolyRPC,
+    createn: PolyN,
+    contractn: PolyA,
+    menun:PolyM
+}
+
+export default envVars
---
+++ pages/index.tsx
@@ -0,0 +1,84 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+
+import { Toaster } from 'react-hot-toast'
+
+import Navbar from '../components/navbar'
+
+
+import { useState, useEffect } from 'react'
+import { CheckCircleIcon } from '@heroicons/react/solid'
+
+
+import Button from '../components/button'
+import Spinner from '../components/spinner'
+import AddressPill from '../components/addressPill'
+import DarkModeToggle from '../components/darkModeToggle'
+
+
+const Home: NextPage = () => {
+  return (
+    <div className="max-w-screen-xl m-auto pb-4 md:pb-12">
+      <Head>
+        <title>Scry Data</title>
+        <meta name="description" content="High Scale, Decentralized Oracles and Data" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Toaster />
+
+      <h1 className="m-auto text-center md:mt-8 text-4xl md:text-6xl font-extrabold">
+        Welcome to{' '}
+        <a href="https://scry.finance" target="_blank" rel="noreferrer" className="rotating-hue">
+        Scry
+        </a>
+      </h1>
+
+
+      <a
+        href="https://github.com/scryprotocol"
+        target="_blank"
+        rel="noreferrer"
+        className="fixed left-5 bottom-4 flex flex-col"
+      >
+        <Image className="bg-white rounded-full" src="/github.png" alt="Github Icon" width="35" height="35" />
+        </a><a
+        href="https://twitter.com/scryprotocol"
+        target="_blank"
+        rel="noreferrer"
+        className="fixed left-16 bottom-4 flex flex-col"
+      >
+        <Image className="bg-white rounded-full" src="/twitter.png" alt="Github Icon" width="35" height="35" />
+      </a>
+      <a
+        href="https://discord.gg/3Z2qvm9BDg"
+        target="_blank"
+        rel="noreferrer"
+        className="fixed left-5 bottom-14 flex flex-col"
+      >
+        <Image className="bg-white rounded-full" src="/discord.svg" alt="Github Icon" width="35" height="35" />
+      </a>
+    
+      <style jsx>{`
+        .rotating-hue {
+          background-image: -webkit-linear-gradient(92deg, #f35626, #feab3a);
+          -webkit-background-clip: text;
+          -webkit-text-fill-color: transparent;
+          -webkit-animation: hue 30s infinite linear;
+        }
+
+        @-webkit-keyframes hue {
+          from {
+            -webkit-filter: hue-rotate(0deg);
+          }
+          to {
+            -webkit-filter: hue-rotate(-360deg);
+          }
+        }
+      `}</style>
+    </div>
+  )
+}
+
+export default Home
